c1f9fb8cfd59f3644377726d01de997d0cae1865
Coursera Learner <nobody@nowhere.nul>
2020-05-15 01:50:37 +0000

    did it

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -1,45 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 size_t maxSeq(int * array, size_t n);
 
 int main() {
-  int array1[] = {2, 3, 4, 2};
-  int array2[] = {2, 1, 3, 4, 5};
-  int array3[] = {-1};
-  int array4[] = {1, 2, 3, 6, 13, 15, 7, -3, 9, 2};
-  int array5[] = {4};
-  int array6[] = {5, 5, 5, 6};
+  int array1[] = {1, 2, 3, 2};
+  int array2[] = {2, -3, 5, 6, 8};
+  int array3[] = {5};
+  int array4[] = {2, 4, 3, 6, 10, 15, -1, 7, 8, 2};
+  int array5[] = {-2};
+  int array6[] = {2,2,2,3};
   if (maxSeq(NULL, 0)) {
     printf("Failed on NULL\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array1, 0)) {
-    printf("Failed on 2, 3, 4, 2 for NULL\n");
+    printf("Failed on 1,2,3,2 for NULL\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array1, 4) != 3) {
-    printf("Failed on 2, 3, 4, 2\n");
+    printf("Failed on 1,2,3,2\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array2, 5) != 4) {
-    printf("Failed on 2, 1, 3, 4, 5\n");
+    printf("Failed on 2,-3,5,6,8\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array3, 1) != 1) {
-    printf("Failed on -1\n");
+    printf("Failed on 5\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array4, 10) != 4) {
-    printf("Failed on 1, 2, 3, 6, 13, 15, 7, -3, 9, 2\n");
+    printf("Failed on 2,4,3,6,10,15,-1,7,8,2\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array5, 1) != 1) {
-    printf("Failed on 4\n");
+    printf("Failed on -2\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array6, 4) != 2) {
-    printf("Failed on 5, 5, 5, 6\n");
+    printf("Failed on 2,2,2,3\n");
     exit(EXIT_FAILURE);
   }
   return EXIT_SUCCESS;
